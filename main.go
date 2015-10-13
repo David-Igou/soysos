a68edd6dc681f8ce42c8243418f59e86fa7ab3e3
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-10-12 21:28:34 -0400

    Catified the API

--- main.go
+++ main.go
@@ -12,10 +12,23 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/cats", CatIndex)
+	router.HandleFunc("/cats/{catFactID}", CatFactShow)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 //Index ayyyy
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w, "Hello Cat Lover, %q", html.EscapeString(r.URL.Path))
+}
+
+func CatIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Cat Index!")
+}
+
+func CatFactShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["catFactID"]
+	fmt.Fprintln(w, "Cat Fact:", todoId)
 }
