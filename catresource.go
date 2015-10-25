4212b8a9b80a713e0f8c3c98845db9c9d3553883
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-10-25 16:17:48 -0400

    updated main.

--- catresource.go
+++ catresource.go
@@ -1,8 +1,68 @@
 package main
 
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/emicklei/go-restful"
+)
+
 type CatFact struct {
+	ID      string `json:"ID"`
 	Species string `json:"species"`
 	Fact    string `json:"fact"`
 }
 
 type CatFacts []CatFact
+
+type CatResource struct {
+	facts map[string]CatFact
+}
+
+func (u CatResource) Register(container *restful.Container) {
+	ws := new(restful.WebService)
+	ws.
+		Path("/cats").
+		Doc("Get some facts about cats :D").
+		Consumes(restful.MIME_JSON).
+		Produces(restful.MIME_JSON) // you can specify this per route as well
+
+	ws.Route(ws.GET("/{user-id}").To(u.serveFact).
+		Doc("get a cat fact!").
+		Operation("serveFact").
+		Param(ws.PathParameter("fact-id", "identifier of the catfact").DataType("string")).
+		Writes(CatFact{})) // on the response
+
+	ws.Route(ws.POST("").To(u.createFact).
+		// docs
+		Doc("create a cat fact!").
+		Operation("createFact").
+		Reads(CatFact{})) // from the request
+
+}
+
+// GET http://localhost:8080/cats/1
+//
+func (u CatResource) serveFact(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("fact-id")
+	cat := u.facts[id]
+	if len(cat.ID) == 0 {
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusNotFound, "404: Fact not found!.")
+		return
+	}
+	response.WriteEntity(cat)
+}
+
+func (u CatResource) createFact(request *restful.Request, response *restful.Response) {
+	cat := new(CatFact)
+	err := request.ReadEntity(cat)
+	if err != nil {
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		return
+	}
+	cat.ID = strconv.Itoa(len(u.facts) + 1) // simple id generation
+	u.facts[cat.ID] = *cat
+	response.WriteHeaderAndEntity(http.StatusCreated, cat)
+}
--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
-import "github.com/emicklei/go-restful"
+import (
+	"log"
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+)
 
 func main() {
-	ws := new(restful.WebService)
-	ws.
-		Path("/cats").
-		Consumes(restful.MIME_JSON).
-		Produces(restful.MIME_JSON)
+	// ws := new(restful.WebService)
+	// ws.
+	// 	Path("/cats").
+	// 	Consumes(restful.MIME_JSON).
+	// 	Produces(restful.MIME_JSON)
+	cat := new(CatResource)
+	wsContainer := restful.NewContainer()
+	cat.Register(wsContainer)
+	server := &http.Server{Addr: ":8080", Handler: wsContainer}
+	//ws.Route(ws.GET("/{cat-id}").To(u.)) // u is a UserResource
 
-	ws.Route(ws.GET("/{cat-id}").To(u.findCat)) // u is a UserResource
+	log.Fatal(server.ListenAndServe())
 }
 
-// GET http://localhost:8080/users/1
-func (u CatResource) findCat(request *restful.Request, response *restful.Response) {
-	id := request.PathParameter("cat-id")
-}
+// GET http://localhost:8080/cats/1
+// func (u CatResource) findCat(request *restful.Request, response *restful.Response) {
+// 	id := request.PathParameter("cat-id")
+// 	u.facts[id].
+// }
