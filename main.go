8f7dbb7a993e1d1f07a33d4ee9344ea8fc3e9e03
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-10-25 21:16:29 -0400

    updated main to initialize a rootresource

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ func main() {
 	cmap := make(map[string]CatFact)
 	cmap["Lion"] = CatFact{"1", "Lion", "Lions have sharp teef :D"}
 	cat := CatResource{cmap}
+	root := RootResource{"Hello"}
 	wsContainer := restful.NewContainer()
 	cat.Register(wsContainer)
+	root.Register(wsContainer)
 	server := &http.Server{Addr: "localhost:8080", Handler: wsContainer}
 	log.Printf("start listening on localhost:8080")
 
