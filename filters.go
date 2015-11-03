36821fa94ce20369a72d0f859fb4515fd7776e62
Gerard C. Briones <brionesgc@vcu.edu>
2015-11-03 14:28:31 -0500

    filters.go 	- Added further detail to WebserviceLogging

--- filters.go
+++ filters.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"time"
+	"strings"
 
 	"github.com/emicklei/go-restful"
 )
@@ -16,18 +17,25 @@ func MeasureTime(req *restful.Request, resp *restful.Response, chain *restful.Fi
 
 // WebService Filter
 func WebserviceLogging(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
-	log.Printf("[SOYSOS (logger)] %s,%s\n", req.Request.Method, req.Request.URL)
+	log.Printf("[SOYSOS (WebserviceLogging)] {%s}, %s - \"%s\" - %s, %d %d\n",
+		strings.Split(req.Request.RemoteAddr, ":")[0],
+		req.Request.Method,
+		req.Request.URL,
+		req.Request.Proto,
+		resp.StatusCode(),
+		resp.ContentLength(),
+	)
 	chain.ProcessFilter(req, resp)
 }
 
 // Global Filter
 func GlobalLogging(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
-	log.Printf("[SOYSOS (log)] %s,%s\n", req.Request.Method, req.Request.URL)
+	log.Printf("[SOYSOS (GlobalLogging)] %s,%s\n", req.Request.Method, req.Request.URL)
 	chain.ProcessFilter(req, resp)
 }
 
 // Route Filter (defines FilterFunction)
 func RouteLogging(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
-	log.Printf("[SOYSOS(logger)] %s,%s\n", req.Request.Method, req.Request.URL)
+	log.Printf("[SOYSOS(RouteLogging)] %s,%s\n", req.Request.Method, req.Request.URL)
 	chain.ProcessFilter(req, resp)
 }
