ce27ac2bc9344856e1b427266f53674ecd0f2cf9
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-11-09 15:04:34 -0500

    database will now create a table for the userinfo if one does exist. InsertUser will return the new session token. FindUser queries for a single row instead of all available

--- database.go
+++ database.go
@@ -20,42 +20,72 @@ type DB struct {
 // );
 
 func Database() *sql.DB {
+
 	db, err := sql.Open("sqlite3", "./users.db")
 	checkErr(err)
+
+	_, err = db.Exec(
+		"CREATE TABLE IF NOT EXISTS users ( id integer PRIMARY KEY, username varchar(64) NOT NULL, password varchar(64), sessionToken varchar(64))")
+
+	checkErr(err)
+
 	return db
 }
 
-func (s DB) InsertUser(u *User) {
+func (s DB) InsertUser(u *User) (string, error) {
 
-	stmt, err := s.db.Prepare("INSERT INTO userinfo(username, password, sessionToken) values(?,?,?)")
+	stmt, err := s.db.Prepare("INSERT INTO users(id, username, password, sessionToken) values(?,?,?,?)")
 	checkErr(err)
 
-	res, err := stmt.Exec(u.Username, u.Password, sessionId())
+	session := sessionId()
+	res, err := stmt.Exec(nil, u.Username, u.Password, session)
 	checkErr(err)
 
 	id, err := res.LastInsertId()
 	checkErr(err)
 
 	log.Println(id)
+	return session, err
 }
 
 func (s DB) FindUser(u *User) {
 	// query
-	rows, err := s.db.Query("SELECT * FROM userinfo")
+	ident := u.ID
+	//var username string
+	row := s.db.QueryRow("SELECT username FROM users WHERE id=?", ident)
+	//row, err := s.db.QueryRow("SELECT username FROM userinfo WHERE id=?", id)
+	//checkErr(err)
+
+	var id []byte
+	var username []byte
+	var password []byte
+	var sessionToken []byte
+
+	err := row.Scan(&id, &username, &password, &sessionToken)
 	checkErr(err)
 
-	for rows.Next() {
-		var sessionToken []byte
-		var username []byte
-		var password []byte
-		var created []byte
-		err = rows.Scan(&sessionToken, &username, &password, &created)
-		checkErr(err)
-		log.Print(string(sessionToken))
-		log.Print(string(username))
-		log.Print(string(password))
-		log.Print(string(created))
+	t := User{
+		string(id),
+		string(username),
+		string(password),
+		string(sessionToken),
 	}
+
+	if t.SessionToken == u.SessionToken {
+		log.Print("wowowowow")
+	}
+	// for row.Next() {
+	// var sessionToken []byte
+	// var username []byte
+	// var password []byte
+	// var created []byte
+	// 	err = row.Scan(&sessionToken, &username, &password, &created)
+	// 	checkErr(err)
+	// 	log.Print(string(sessionToken))
+	// 	log.Print(string(username))
+	// 	log.Print(string(password))
+	// 	log.Print(string(created))
+	// }
 
 }
 
