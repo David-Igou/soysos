0a5f5e79ac30db17fe51f62d0f3fab0260a344f6
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-11-18 13:41:54 -0500

    added swagger ui

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/emicklei/go-restful"
+	"github.com/emicklei/go-restful/swagger"
+	"github.com/magiconair/properties"
+)
+
+var (
+	props          *properties.Properties
+	propertiesFile = flag.String("config", "soysos.properties", "the configuration file")
+	SwaggerPath    string
 )
 
 func main() {
+	flag.Parse()
+
+	log.Printf("loading configuration from [%s]", *propertiesFile)
+	var err error
+	if props, err = properties.LoadFile(*propertiesFile, properties.UTF8); err != nil {
+		log.Fatalf("[mora][error] Unable to read properties:%v\n", err)
+	}
+
 	cmap := make(map[string]CatFact)
 	cmap["Lion"] = CatFact{"", "Lion", "Lions have sharp teef :D"}
 
 	cat := CatResource{cmap}
-	root := RootResource{"Hello"}
+	root := RootResource{}
 	user := UserResource{}
 
 	wsContainer := restful.NewContainer()
@@ -21,6 +38,16 @@ func main() {
 	cat.Register(wsContainer)
 	root.Register(wsContainer)
 	user.Register(wsContainer)
+	SwaggerPath = props.GetString("swagger.path", "")
+
+	config := swagger.Config{
+		WebServices:     wsContainer.RegisteredWebServices(),
+		WebServicesUrl:  "http://localhost:8008",
+		ApiPath:         "/apidocs.json",
+		SwaggerPath:     SwaggerPath,
+		SwaggerFilePath: "./dist"}
+
+	swagger.RegisterSwaggerService(config, wsContainer)
 
 	server := &http.Server{Addr: "localhost:8008", Handler: wsContainer}
 
